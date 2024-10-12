68b7b000eb7e02fe9b2d2754a9380974e50c7ad1
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-10-12 11:14:57 +0300

    first commit

--- src/java/Movie.java
+++ src/java/Movie.java
@@ -1,41 +1,41 @@
-public class Movie{
+public class Movie {
 
-    private String title ;
-    private String director ;
-    private String genre ;
-    private int releaseYear ;
-    private double rating ;
+    private String title;
+    private String director;
+    private String genre;
+    private int releaseYear;
+    private double rating;
 
-    public Movie (String title, String director, String genre, int releaseYear, double rating){
-        this.title = title ;
-        this.director = director ;
-        this.genre = genre ;
-        this.releaseYear = releaseYear ;
-        this.rating = rating ;
+    public Movie(String title, String director, String genre, int releaseYear, double rating) {
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.rating = rating;
     }
 
-    public String getTitle (){
-        return title ;
+    public String getTitle() {
+        return title;
     }
 
-    public String getDirector (){
-        return director ;
+    public String getDirector() {
+        return director;
     }
 
-    public String getGenre (){
-        return genre ;
+    public String getGenre() {
+        return genre;
     }
 
-    public int getReleaseYear (){
-        return releaseYear ;
+    public int getReleaseYear() {
+        return releaseYear;
     }
 
-    public double getRating (){
-        return rating ;
+    public double getRating() {
+        return rating;
     }
 
     @Override
-    public String toString (){
-        return title + " (" + releaseYear + ") - Directed by " + director + ", Genre: " + genre + ", Rating: " + rating ;
+    public String toString() {
+        return title + " (" + releaseYear + ") - Directed by " + director + ", Genre: " + genre + ", Rating: " + rating;
     }
 }
