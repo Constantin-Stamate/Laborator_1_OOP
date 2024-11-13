0d5df6986d14e4b8660df1c6faa73d0286c1be87
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-14 00:32:09 +0200

    add examples of filtering, sorting and retrieving top-rated movies

--- src/java/Main.java
+++ src/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Movie movie1 = new Movie("Inception", "Christopher Nolan", "Sci-Fi", 2010, 8.8);
         Movie movie2 = new Movie("The Dark Knight", "Christopher Nolan", "Action", 2008, 9.0);
         Movie movie3 = new Movie("The Matrix", "Lana Wachowski, Lilly Wachowski", "Sci-Fi", 1999, 8.7);
-        Movie movie4 = new Movie("Pulp Fiction", "Quentin Tarantino", "Crime", 1994, 8.9);
+        Movie movie4 = new Movie("Fiction", "Quentin Tarantino", "Crime", 1994, 8.9);
 
         movieStore.addMovie(movie1);
         movieStore.addMovie(movie2);
