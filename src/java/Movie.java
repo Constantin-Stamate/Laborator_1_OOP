4bd8c16455f217a672a10bd3cd37f63e398ce61c
Constantin_Stamate <constantinstamate.r@gmail.com>
2024-11-14 00:18:13 +0200

    create movie class

--- src/java/Movie.java
+++ src/java/Movie.java
@@ -36,6 +36,6 @@ public class Movie {
 
     @Override
     public String toString() {
-        return title + " (" + releaseYear + ") - Directed by " + director + ", Genre: " + genre + ", Rating: " + rating;
+        return title + " (" + releaseYear + ") - Directed by: " + director + ", Genre: " + genre + ", Rating: " + rating;
     }
 }
